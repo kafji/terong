8d385e56b3817d72c00f2788bd25842be63d118a
Kafji <k@kafji.net>
2024-11-02 13:53:27 +0700

    cpuprof

--- go/cmd/terong-client/main.go
+++ go/cmd/terong-client/main.go
@@ -4,11 +4,24 @@ package main
 
 import (
 	"context"
+	"os"
+	"runtime/pprof"
 
 	"kafji.net/terong/terong/client"
 )
 
 func main() {
 	ctx := context.Background()
+
+	f, err := os.Create("terong-client.prof")
+	if err != nil {
+		panic(err)
+	}
+	err = pprof.StartCPUProfile(f)
+	if err != nil {
+		panic(err)
+	}
+	defer pprof.StopCPUProfile()
+
 	client.Start(ctx)
 }
--- go/cmd/terong-server/main.go
+++ go/cmd/terong-server/main.go
@@ -4,11 +4,24 @@ package main
 
 import (
 	"context"
+	"os"
+	"runtime/pprof"
 
 	"kafji.net/terong/terong/server"
 )
 
 func main() {
 	ctx := context.Background()
+
+	f, err := os.Create("terong-server.prof")
+	if err != nil {
+		panic(err)
+	}
+	err = pprof.StartCPUProfile(f)
+	if err != nil {
+		panic(err)
+	}
+	defer pprof.StopCPUProfile()
+
 	server.Start(ctx)
 }
