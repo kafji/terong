474a015db283def2a3d859019216bece27c7297d
Kafji <k@kafji.net>
2024-11-14 19:55:38 +0700

    min size and handle found no files

--- rust/terong/src/bin/rf.rs
+++ rust/terong/src/bin/rf.rs
@@ -1,10 +1,12 @@
-use anyhow::{Context, Error as Anyhow};
+use anyhow::{anyhow, Context, Error as Anyhow};
 use rand::{thread_rng, Rng};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
     fs::{read_dir, DirEntry},
     path::PathBuf,
     process::ExitCode,
+    str::FromStr,
     sync::{
         atomic::{AtomicUsize, Ordering},
         Arc, Mutex, Weak,
@@ -16,29 +18,43 @@ fn main() -> ExitCode {
 
     let name = args.next().unwrap();
 
-    match args.next() {
-        None => {
-            eprintln!("Select random file.\nUsage: {} <path>", name);
+    match (args.next(), args.next()) {
+        (None, _) => {
+            eprintln!("Select random file.\nUsage: {} <path> [min size]", name);
             ExitCode::FAILURE
         }
-        Some(path) => match select_file(path) {
-            Ok(file_path) => {
-                println!("{}", file_path.display());
-                ExitCode::SUCCESS
+        (Some(path), min_size) => {
+            match min_size
+                .map(|x| {
+                    x.parse()
+                        .map_err(|err| anyhow!("unexpected min size: {}", err))
+                })
+                .transpose()
+                .and_then(|min_size| select_file(path, min_size))
+            {
+                Ok(_) => ExitCode::SUCCESS,
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                    ExitCode::FAILURE
+                }
             }
-            Err(err) => {
-                eprintln!("Error: {}", err);
-                ExitCode::FAILURE
-            }
-        },
+        }
     }
 }
 
-fn select_file(path: String) -> Result<PathBuf, Anyhow> {
-    let tree = build_tree(path)?;
+fn select_file(path: String, min_size: Option<HuByte>) -> Result<(), Anyhow> {
+    if let Some(min_size) = min_size.as_ref() {
+        println!("Min size: {}.", min_size);
+    }
+
+    let tree = build_tree(path, min_size)?;
 
     let count = tree.count.load(Ordering::Relaxed);
     println!("Found {} files.", count);
+
+    if count == 0 {
+        return Ok(());
+    }
 
     let mut files = Tree::files(tree);
 
@@ -46,7 +62,9 @@ fn select_file(path: String) -> Result<PathBuf, Anyhow> {
     let file = files.nth(n).unwrap();
 
     let path = file.lock().unwrap().path.clone();
-    Ok(path)
+    println!("{}", path.display());
+
+    Ok(())
 }
 
 #[allow(unused)]
@@ -98,18 +116,28 @@ impl Node {
     fn new_child_from_dir_entry(
         parent: &Arc<Mutex<Node>>,
         entry: &DirEntry,
+        min_size: Option<HuByte>,
     ) -> Result<Option<Arc<Mutex<Self>>>, Anyhow> {
+        let name = entry.file_name().to_string_lossy().into_owned();
+
+        const BLACKLIST: &[&str] = &["$RECYCLE.BIN", "System Volume Information"];
+        if BLACKLIST.contains(&name.as_str()) {
+            return Ok(None);
+        }
+
         let ftype = entry.file_type()?;
 
         if !ftype.is_dir() && !ftype.is_file() {
             return Ok(None);
         }
 
-        let name = entry.file_name().to_string_lossy().into_owned();
-
-        const BLACKLIST: &[&str] = &["$RECYCLE.BIN", "System Volume Information"];
-        if BLACKLIST.contains(&name.as_str()) {
-            return Ok(None);
+        if ftype.is_file() {
+            if let Some(min_size) = min_size {
+                let fmeta = entry.metadata()?;
+                if fmeta.len() < min_size.to_u64() {
+                    return Ok(None);
+                }
+            }
         }
 
         let tree = parent.lock().unwrap().tree.clone();
@@ -135,7 +163,7 @@ impl Tree {
     }
 }
 
-fn build_tree(root_path: String) -> Result<Arc<Tree>, Anyhow> {
+fn build_tree(root_path: String, min_size: Option<HuByte>) -> Result<Arc<Tree>, Anyhow> {
     let mut tree = Tree {
         root: None,
         count: Arc::new(AtomicUsize::new(0)),
@@ -153,14 +181,14 @@ fn build_tree(root_path: String) -> Result<Arc<Tree>, Anyhow> {
         .with_context(|| format!("failed to read root directory {}", root_path))?
     {
         let entry = entry?;
-        Node::new_child_from_dir_entry(&root, &entry)?;
+        Node::new_child_from_dir_entry(&root, &entry, min_size)?;
     }
 
     let mut leaves = root.lock().unwrap().children.clone();
     while !leaves.is_empty() {
         leaves = leaves
             .into_par_iter()
-            .map(|leaf| expand_leaf(leaf))
+            .map(|leaf| expand_leaf(leaf, min_size))
             .reduce(
                 || Ok(Vec::new()),
                 |acc, more| match (acc, more) {
@@ -176,7 +204,10 @@ fn build_tree(root_path: String) -> Result<Arc<Tree>, Anyhow> {
     Ok(tree)
 }
 
-fn expand_leaf(node: Arc<Mutex<Node>>) -> Result<Vec<Arc<Mutex<Node>>>, Anyhow> {
+fn expand_leaf(
+    node: Arc<Mutex<Node>>,
+    min_size: Option<HuByte>,
+) -> Result<Vec<Arc<Mutex<Node>>>, Anyhow> {
     if !node.lock().unwrap().dir {
         return Ok(Vec::new());
     }
@@ -195,7 +226,7 @@ fn expand_leaf(node: Arc<Mutex<Node>>) -> Result<Vec<Arc<Mutex<Node>>>, Anyhow> 
 
     for entry in dir {
         let entry = entry?;
-        Node::new_child_from_dir_entry(&node, &entry)?;
+        Node::new_child_from_dir_entry(&node, &entry, min_size)?;
     }
 
     let children = node.lock().unwrap().children.clone();
@@ -233,5 +264,90 @@ impl Iterator for Files {
         self.trail.extend(last.lock().unwrap().children.clone());
 
         self.next()
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum HuByteUnit {
+    KB,
+    MB,
+    GB,
+}
+
+impl FromStr for HuByteUnit {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "k" => Ok(Self::KB),
+            "m" => Ok(Self::MB),
+            "g" => Ok(Self::GB),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Display for HuByteUnit {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        f.write_str(match self {
+            HuByteUnit::KB => "KB",
+            HuByteUnit::MB => "MB",
+            HuByteUnit::GB => "GB",
+        })
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct HuByte {
+    val: u64,
+    unit: HuByteUnit,
+}
+
+impl HuByte {
+    fn to_u64(&self) -> u64 {
+        self.val
+            * match self.unit {
+                HuByteUnit::KB => 1024,
+                HuByteUnit::MB => 1024 * 1024,
+                HuByteUnit::GB => 1024 * 1024 * 1024,
+            }
+    }
+}
+
+impl FromStr for HuByte {
+    type Err = Anyhow;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let err = || anyhow!("expecting <number><unit> where unit is one of k, m, or g");
+
+        let mut chars = s.chars();
+
+        let mut digits = String::new();
+        let mut unit = String::new();
+        while let Some(c) = chars.next() {
+            if !c.is_ascii_digit() {
+                unit.push(c);
+                break;
+            }
+            digits.push(c);
+        }
+        unit.extend(chars);
+
+        let digits = digits.parse().map_err(|_| err())?;
+
+        let unit = unit.parse().map_err(|_| err())?;
+
+        Ok(Self { val: digits, unit })
+    }
+}
+
+impl Display for HuByte {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        f.write_fmt(format_args!(
+            "{} {} ({})",
+            self.val,
+            self.unit,
+            self.to_u64()
+        ))
     }
 }
