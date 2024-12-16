6c9ff3b47c1913041dafc982559a5e3b1249c3df
Kafji <k@kafji.net>
2024-12-16 13:00:23 +0700

    remove profiling

--- go/cmd/terong-client/main.go
+++ go/cmd/terong-client/main.go
@@ -4,11 +4,7 @@ package main
 
 import (
 	"context"
-	"errors"
 	"os"
-	"os/signal"
-	"runtime/pprof"
-	"syscall"
 
 	"kafji.net/terong/logging"
 	"kafji.net/terong/terong/client"
@@ -20,29 +16,6 @@ func main() {
 	slog.Info("starting", "GOGC", os.Getenv("GOGC"), "GODEBUG", os.Getenv("GODEBUG"), "GOTRACEBACK", os.Getenv("GOTRACEBACK"))
 
 	ctx := context.Background()
-
-	f, err := os.Create("terong-client.prof")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	err = pprof.StartCPUProfile(f)
-	if err != nil {
-		panic(err)
-	}
-	defer pprof.StopCPUProfile()
-
-	ctx, cancel := context.WithCancelCause(ctx)
-	defer cancel(nil)
-
-	s := make(chan os.Signal, 1)
-	signal.Notify(s, syscall.SIGINT)
-	go func() {
-		defer pprof.StopCPUProfile()
-		defer cancel(errors.New("SIGINT"))
-		<-s
-		signal.Stop(s)
-	}()
 
 	client.Start(ctx)
 }
--- go/cmd/terong-server/main.go
+++ go/cmd/terong-server/main.go
@@ -4,11 +4,7 @@ package main
 
 import (
 	"context"
-	"errors"
 	"os"
-	"os/signal"
-	"runtime/pprof"
-	"syscall"
 
 	"kafji.net/terong/logging"
 	"kafji.net/terong/terong/server"
@@ -20,29 +16,6 @@ func main() {
 	slog.Info("starting", "GOGC", os.Getenv("GOGC"), "GODEBUG", os.Getenv("GODEBUG"), "GOTRACEBACK", os.Getenv("GOTRACEBACK"))
 
 	ctx := context.Background()
-
-	f, err := os.Create("terong-server.prof")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	err = pprof.StartCPUProfile(f)
-	if err != nil {
-		panic(err)
-	}
-	defer pprof.StopCPUProfile()
-
-	ctx, cancel := context.WithCancelCause(ctx)
-	defer cancel(nil)
-
-	s := make(chan os.Signal, 1)
-	signal.Notify(s, syscall.SIGINT)
-	go func() {
-		defer pprof.StopCPUProfile()
-		defer cancel(errors.New("SIGINT"))
-		<-s
-		signal.Stop(s)
-	}()
 
 	server.Start(ctx)
 }
