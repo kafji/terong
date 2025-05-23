34f85a3a57e6a489f360390ad53ecea19dccbe4e
Kafji <k@kafji.net>
2025-05-23 07:02:38 +0700

    reduce max parallelism

--- go/cmd/terong-client/main.go
+++ go/cmd/terong-client/main.go
@@ -4,18 +4,14 @@ package main
 
 import (
 	"context"
-	"os"
+	"runtime"
 
-	"kafji.net/terong/logging"
 	"kafji.net/terong/terong/client"
 )
 
-var slog = logging.NewLogger("terong-client/main")
-
 func main() {
-	slog.Info("starting", "GOGC", os.Getenv("GOGC"), "GODEBUG", os.Getenv("GODEBUG"), "GOTRACEBACK", os.Getenv("GOTRACEBACK"))
+	runtime.GOMAXPROCS(max(runtime.NumCPU()/2, 2))
 
 	ctx := context.Background()
-
 	client.Start(ctx)
 }
--- go/cmd/terong-server/main.go
+++ go/cmd/terong-server/main.go
@@ -4,18 +4,14 @@ package main
 
 import (
 	"context"
-	"os"
+	"runtime"
 
-	"kafji.net/terong/logging"
 	"kafji.net/terong/terong/server"
 )
 
-var slog = logging.NewLogger("terong-server/main")
-
 func main() {
-	slog.Info("starting", "GOGC", os.Getenv("GOGC"), "GODEBUG", os.Getenv("GODEBUG"), "GOTRACEBACK", os.Getenv("GOTRACEBACK"))
+	runtime.GOMAXPROCS(max(runtime.NumCPU()/2, 2))
 
 	ctx := context.Background()
-
 	server.Start(ctx)
 }
