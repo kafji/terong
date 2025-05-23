2731b6287120c9ab1b798f00f408a9ea42dfdafc
Kafji <k@kafji.net>
2025-05-23 18:32:05 +0700

    better obfuscated file name

--- rust/crates/terong/src/bin/terong-obfuscate.rs
+++ rust/crates/terong/src/bin/terong-obfuscate.rs
@@ -1,3 +1,4 @@
+use std::{path::PathBuf, str::FromStr};
 use terong::{
     EVENT_LOG_FILE_PATH,
     event_logger::{LocalInputEventObfuscator, obfuscate},
@@ -6,8 +7,16 @@ use tokio::fs::File;
 
 #[tokio::main]
 async fn main() {
-    let input = File::open(EVENT_LOG_FILE_PATH).await.unwrap();
-    let output = File::create(&format!("{}.obfuscated", EVENT_LOG_FILE_PATH)).await.unwrap();
+    let mut path = PathBuf::from_str(EVENT_LOG_FILE_PATH).unwrap();
+    let input = File::open(&path).await.unwrap();
+
+    path.set_file_name(format!(
+        "{}.obfuscated.{}",
+        path.file_stem().expect("missing file stem").to_string_lossy(),
+        path.extension().expect("missing extension").to_string_lossy()
+    ));
+    let output = File::create(&path).await.unwrap();
+
     obfuscate(input, output, LocalInputEventObfuscator::new())
         .await
         .unwrap();
