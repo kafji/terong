9bdd7ac96869e4deab5e8e2fefe83a17fe54ed0d
Kafji <k@kafji.net>
2025-05-28 03:56:14 +0700

    default gomaxprocs

--- go/cmd/terong-client/main.go
+++ go/cmd/terong-client/main.go
@@ -4,14 +4,11 @@ package main
 
 import (
 	"context"
-	"runtime"
 
 	"kafji.net/terong/terong/client"
 )
 
 func main() {
-	runtime.GOMAXPROCS(max(runtime.NumCPU()/2, 2))
-
 	ctx := context.Background()
 	client.Start(ctx)
 }
--- go/cmd/terong-server/main.go
+++ go/cmd/terong-server/main.go
@@ -4,14 +4,11 @@ package main
 
 import (
 	"context"
-	"runtime"
 
 	"kafji.net/terong/terong/server"
 )
 
 func main() {
-	runtime.GOMAXPROCS(max(runtime.NumCPU()/2, 2))
-
 	ctx := context.Background()
 	server.Start(ctx)
 }
