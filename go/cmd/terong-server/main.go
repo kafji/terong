bb5e6067a2739a86f36d175d672c108fa11be3cc
Kafji <k@kafji.net>
2025-05-30 07:51:42 +0700

    add http/pprof

--- go/cmd/terong-server/main.go
+++ go/cmd/terong-server/main.go
@@ -4,11 +4,20 @@ package main
 
 import (
 	"context"
+	"net/http"
+	_ "net/http/pprof"
 
 	"kafji.net/terong/terong/server"
 )
 
 func main() {
 	ctx := context.Background()
+
+	go func() {
+		if err := http.ListenAndServe("127.0.0.1:5555", nil); err != nil {
+			panic(err)
+		}
+	}()
+
 	server.Start(ctx)
 }
